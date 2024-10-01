aa90b50529550cf87071740a7008580ec2b9f38a
rafaellore <birdneon3@gmail.com>
2024-10-01 15:27:14 -0300

    feat(form): adds test about invalid params on inputs

--- cypress/e2e/formulárioCadastro.cy.js
+++ cypress/e2e/formulárioCadastro.cy.js
@@ -16,4 +16,17 @@ describe('Formulário Cadastro', () => {
       .should('exist')
       .and('have.text', 'Usuário cadastrado com sucesso!');
   });
+
+  it('Não deve permitir o cadastro de usuários com email e senha inválido', () => {
+    cy.getByData('botao-cadastro').click();
+
+    cy.getByData('email-input').type('moni@alura.com');
+    cy.getByData('senha-input').type('987654');
+
+    cy.getByData('botao-enviar').click();
+
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O campo de nome é obrigatório');
+  });
 });
