c3a09eae6fad30e28099ea0ef517aa805e9b37a4
rafaellore <birdneon3@gmail.com>
2024-10-10 16:37:56 -0300

    test(páginas): adding pathname tests

--- cypress/e2e/ páginas.cy.js
+++ cypress/e2e/ páginas.cy.js
@@ -9,10 +9,14 @@ describe('Testando múltiplas páginas', () => {
 
     cy.getByData('botao-enviar').click();
 
+    cy.location('pathname').should('eq', '/home');
+
     cy.getByData('app-home').find('a').eq(1).click();
 
     cy.getByData('titulo-cartoes')
       .should('exist')
       .and('have.text', 'Meus cartões');
+
+    cy.location('pathname').should('eq', '/home/cartoes');
   });
 });
